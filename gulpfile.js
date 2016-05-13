4c13b4663b4ab8d32ffe0d7c842ec79bcd23f54a
Ali Sinan Köksal <alisinan@gmail.com>
2016-05-13 14:06:42 -0700

    added gulp task to deploy to github pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
