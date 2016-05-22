ac9b833cb4a0f924460bfce2d33767885c37bc1f
Ali Sinan Köksal <alisinan@gmail.com>
2016-05-21 21:40:59 -0700

    added gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var del = require('del');
+
+var config = {
+  entryFile: './src/main.js',
+  outputDir: './dist/',
+  outputFile: 'js/app.js'
+};
+
+gulp.task('clean', function () {
+  return del(['dist']);
+});
 
 gulp.task('default', function () {
 
@@ -16,12 +29,12 @@ gulp.task('serve', function () {
     server: {
       baseDir: './'
     },
+    port: 8000,
     ui: {
       port: 8001,
       weinre: {
         port: 8002
       }
-    },
-    port: 8000
+    }
   });
 });
