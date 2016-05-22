4f9fdb899de38d0cac4bb23fdb2d15e515e61970
Ali Sinan Köksal <alisinan@gmail.com>
2016-05-21 19:00:19 -0700

    added gulp task to serve the page

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,27 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var browserSync = require('browser-sync');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
 
 });
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function () {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
+});
+
+gulp.task('serve', function () {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    ui: {
+      port: 8001,
+      weinre: {
+        port: 8002
+      }
+    },
+    port: 8000
+  });
 });
